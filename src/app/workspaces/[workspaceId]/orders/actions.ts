aa9b6ed2085afe98dd9ca4de5537fe5fffbccd2a
Wichan Kang <wichan7@naver.com>
2025-12-07 15:23:18 +0900

    feat: Toast 기능 추가 (#15)

--- src/app/workspaces/[workspaceId]/orders/actions.ts
+++ src/app/workspaces/[workspaceId]/orders/actions.ts
@@ -1,24 +1,20 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import orderService from "@/services/orders/service";
 import type { Order } from "@/services/orders/types";
 
 export async function createOrderAction(order: Partial<Order>) {
   await orderService.create(order as Order);
-  redirect("..");
 }
 
 export async function modifyOrderAction(order: Partial<Order>) {
   await orderService.modify(order as Order);
-  redirect("..");
 }
 
 export async function removeOrderAction(id: Order["id"]) {
   if (!id) throw new Error("Invalid id");
 
   await orderService.remove(id);
-  redirect("..");
 }
 
 export const getOrdersAction = async (workspaceId: string) => {
