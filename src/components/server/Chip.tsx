dd658ad048e5e3e7d2453c479d77b6744f075db0
Wichan Kang <wichan7@naver.com>
2025-12-26 12:47:37 +0900

    style: 색약 지원 (#21)

--- src/components/server/Chip.tsx
+++ src/components/server/Chip.tsx
@@ -1,14 +1,19 @@
 import type React from "react";
 
+type ChipSize = "small" | "medium";
+
 interface ChipProps {
   children: React.ReactNode;
   className?: string;
+  size?: ChipSize;
 }
 
-export default function Chip({ children, className }: ChipProps) {
+export default function Chip({ children, className, size = "medium" }: ChipProps) {
   return (
     <div
-      className={`truncate px-3 py-1 bg-gray-200 text-gray-800 rounded-full text-sm ${className}`}
+      className={`truncate px-3 py-1 bg-gray-200 text-gray-800 rounded-full text-sm ${className} ${
+        size === "small" ? "text-xs" : "text-sm"
+      }`}
     >
       {children}
     </div>
